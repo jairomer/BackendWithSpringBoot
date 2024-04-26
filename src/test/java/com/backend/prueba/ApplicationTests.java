a5a95895410b84ea1853e3466a78aa56ec2f42a8
synk <jairomer@protonmail.com>
2024-04-26 17:45:28 +0200

    Hamcrest is operational

--- src/test/java/com/backend/prueba/ApplicationTests.java
+++ src/test/java/com/backend/prueba/ApplicationTests.java
@@ -1,13 +1,22 @@
 package com.backend.prueba;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.core.IsNull.notNullValue;; 
+
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private BaseController baseController;
+
 	@Test
 	void contextLoads() {
+		assertThat(baseController, notNullValue());
 	}
 
 }
