26ba0697fe477eafbf3f0c2b172b60d711d09217
synk <jairomer@protonmail.com>
2024-04-26 18:34:49 +0200

    restassured is installed and can be used to make integration tests.

--- src/test/java/com/backend/prueba/ApplicationTests.java
+++ src/test/java/com/backend/prueba/ApplicationTests.java
@@ -1,22 +1,46 @@
 package com.backend.prueba;
 
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.text.StringContainsInOrder.stringContainsInOrder;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.hamcrest.MatcherAssert.assertThat; 
-import static org.hamcrest.core.IsNull.notNullValue;; 
+import io.restassured.RestAssured; 
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Autowired
 	private BaseController baseController;
-
+	
 	@Test
 	void contextLoads() {
 		assertThat(baseController, notNullValue());
+	}
+
+	@Test
+	void testHelloWorld() {
+		String responseBody = given()
+		.port(port)
+		.get("/").then()
+		.statusCode(200)
+		.extract()
+		.body()
+		.asString();
+		
+		assertThat(responseBody, stringContainsInOrder("Hello, World!"));
 	}
 
 }
